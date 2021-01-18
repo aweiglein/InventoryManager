805dc260e55330a62a769cea3c4399b241a57149
aweiglein <67802012+aweiglein@users.noreply.github.com>
2021-01-18 10:38:43 -0800

    Update Inventory.java

--- Model/Inventory.java
+++ Model/Inventory.java
@@ -4,84 +4,164 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
- * Class creates Inventory
+ * Creates Inventory
  */
 public class Inventory {
-
     private static ObservableList<Part> allParts = FXCollections.observableArrayList();
     private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
 
+    /**
+     * Adds part to inventory
+     * @param newPart new part to be added to inventory
+     */
     public static void addPart(Part newPart) {
-        allParts.add(newPart);
-    }
-    public static void addProduct(Product newProduct) {
-        allProducts.add(newProduct);
+        if (newPart != null) {
+            allParts.add(newPart);
+        }
     }
 
-    public static Part lookupPart(int partId) {
-        for (Part part : allParts ) {
-            if (part.getID() == partId) {
-                return part;
+    /**
+     * Looks up part by part ID
+     * @param partID part ID
+     * @return part if found by part ID
+     */
+    public static Part lookupPart(int partID) {
+        if (!allParts.isEmpty()) {
+            for (int i = 0; i < allParts.size(); i++) {
+                if (allParts.get(i).getPartID() == partID) {
+                    return allParts.get(i);
+                }
             }
         }
         return null;
     }
 
-    public static Product lookupProduct(int productId) {
-        for (Product product : allProducts ) {
-            if (product.getID() == productId) {
-                return product;
+    /**
+     * Looks up part by part name
+     * @param partName part name
+     * @return part if found by part name
+     */
+    public static Part lookupPart(String partName) {
+        if (!allParts.isEmpty()) {
+            for (int i = 0; i < allParts.size(); i++) {
+                if (allParts.get(i).getName() == partName) {
+                    return allParts.get(i);
+                }
             }
         }
         return null;
     }
 
-    public static ObservableList<Part> lookupPart(String partName) {
-        ObservableList<Part> foundParts = FXCollections.observableArrayList();
+    /**
+     * Updates selected part
+     * @param selectedPart part to update
+     */
+    public static void updatePart(Part selectedPart) {
         for (int i = 0; i < allParts.size(); i++) {
-            if (allParts.get(i).getName().toLowerCase().contains(partName.toLowerCase())) {
-                foundParts.add(allParts.get(i));
+            if (allParts.get(i).getPartID() == selectedPart.getPartID()) {
+                allParts.set(i, selectedPart);
+                break;
             }
         }
-        return  foundParts;
-
     }
 
-    public static ObservableList<Product> lookupProduct(String productName) {
-        ObservableList<Product> foundProducts = FXCollections.observableArrayList();
-        for (int i = 0; i < allProducts.size(); i++) {
-            if (allProducts.get(i).getName().toLowerCase().contains(productName.toLowerCase())) {
-                foundProducts.add(allProducts.get(i));
-            }
-        }
-        return  foundProducts;
-    }
-
-    public int getPartIndex(Part part) {
-        return allParts.indexOf(part);
-    }
-
-    public static void updatePart(int index, Part selectedPart) {
-        allParts.set(index, selectedPart);
-    }
-
-    public static void updateProduct(int index, Product selectedProduct) {
-        allProducts.set(index, selectedProduct);
-    }
-
+    /**
+     * Deletes selected part
+     * @param selectedPart part to be deleted
+     * @return boolean value
+     */
     public static boolean deletePart(Part selectedPart) {
-        allParts.remove(selectedPart);
-        return true;
+        for (int i = 0; i < allParts.size(); i++) {
+            if (allParts.get(i).getPartID() == selectedPart.getPartID()) {
+                allParts.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
 
+    /**
+     * Adds product to inventory
+     * @param newProduct new product to be added to inventory
+     */
+    public static void addProduct(Product newProduct) {
+        if (newProduct != null) {
+            allProducts.add(newProduct);
+        }
+    }
+
+    /**
+     * Looks up product by product ID
+     * @param productID product ID
+     * @return product if found by product ID
+     */
+    public static Product lookupProduct(int productID) {
+        if (!allProducts.isEmpty()) {
+            for (int i = 0; i < allProducts.size(); i++) {
+                if (allProducts.get(i).getProductID() == productID) {
+                    return allProducts.get(i);
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Looks up product by product name
+     * @param productName product name
+     * @return product if found by product name
+     */
+    public static Product lookupProduct(String productName) {
+        if (!allProducts.isEmpty()) {
+            for (int i = 0; i < allProducts.size(); i++) {
+                if (allProducts.get(i).getName() == productName) {
+                    return allProducts.get(i);
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Updates product
+     * @param selectedProduct product to update
+     */
+    public static void updateProduct(Product selectedProduct) {
+        for (int i = 0; i < allProducts.size(); i++) {
+            if (allProducts.get(i).getProductID() == selectedProduct.getProductID()) {
+                allProducts.set(i, selectedProduct);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Deletes selected product
+     * @param selectedProduct product to be deleted
+     * @return boolean value
+     */
     public static boolean deleteProduct(Product selectedProduct) {
-        allProducts.remove(selectedProduct);
-        return true;
+        for (int i = 0; i < allProducts.size(); i++) {
+            if (allProducts.get(i).getProductID() == selectedProduct.getProductID()) {
+                allProducts.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
 
+    /**
+     * Getter for all parts in inventory
+     * @return list of parts
+     */
     public static ObservableList<Part> getAllParts() {
         return allParts;
     }
+
+    /**
+     * Getter for all products in inventory
+     * @return list of products
+     */
     public static ObservableList<Product> getAllProducts() {
         return allProducts;
     }
