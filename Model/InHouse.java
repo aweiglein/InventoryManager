29d27190b2540397bc0d08bb7f0a80af2bc3c9cd
aweiglein <67802012+aweiglein@users.noreply.github.com>
2021-01-18 10:38:03 -0800

    Update InHouse.java

--- Model/InHouse.java
+++ Model/InHouse.java
@@ -1,21 +1,43 @@
 package Model;
 
 /**
- * Class creates In-House Parts
+ * Creates In-House parts
  */
 public class InHouse extends Part {
 
     private int machineID;
 
-    public InHouse(int ID, String name, double price, int stock, int min, int max, int machineID) {
-        super(ID, name, price, stock, min, max);
+    /**
+     * Class constructor
+     * @param partID part ID
+     * @param name part name
+     * @param price price of the product
+     * @param stock amount of inventory
+     * @param min minimum amount
+     * @param max maximum amount
+     * @param machineID machine ID
+     */
+    public InHouse(int partID, String name, double price, int stock, int min, int max, int machineID) {
+        setPartID(partID);
+        setName(name);
+        setPrice(price);
+        setStock(stock);
+        setMin(min);
+        setMax(max);
         this.machineID = machineID;
     }
 
+    /**
+     * Getter for machine ID
+     * @return machine ID
+     */
+    public int getMachineID() { return machineID; }
+
+    /**
+     * Setter for machine ID
+     * @param machineID machine ID
+     */
     public void setMachineID(int machineID) {
         this.machineID = machineID;
-    }
-    public int getMachineID() {
-        return machineID;
     }
 }
