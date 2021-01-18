381fae347c27a8999dbf024a6b870ef0c1defdb8
aweiglein <67802012+aweiglein@users.noreply.github.com>
2021-01-18 10:37:34 -0800

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -1,3 +1,8 @@
+/**
+ * @author Alyssa Weiglein
+ * Inventory Management System
+ */
+
 package Main;
 
 import Model.Inventory;
@@ -6,43 +11,44 @@ import Model.Outsourced;
 import Model.Product;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
- * @author Alyssa Weiglein
- * For a future update, I would update the program to require a check if parts are being used in products before being allowed to delete them.
- */
-
-/**
- * Main class starts application
+ * Controller for the 'Main' screen
  */
 public class Main extends Application {
 
+    /**
+     * Launches Java application
+     * @param args command line arguments
+     */
     public static void main(String[] args) { launch(args); }
 
+    /**
+     * Loads the 'Main' screen
+     */
     @Override
-    public void init() {
-        System.out.println("Opening Inventory Management System");
-    }
+    public void start(Stage primaryStage) throws Exception{
+        Inventory inv = new Inventory();
+        addTestData(inv);
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Inventory inventory = new Inventory();
-        addTestData(inventory);
-
-        Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/MainScreen.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View_Controller/Main.fxml"));
+        View_Controller.MainController controller = new View_Controller.MainController(inv);
+        loader.setController(controller);
+        Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
     /**
-     * Method creates test data
+     * Loads the test 'Inventory' data
      */
-    public void addTestData(Inventory inventory) {
-        //In-house parts
+    public void addTestData(Inventory inv) {
+        //In-House parts
         InHouse partA = new InHouse(1, "Part A", 3.99, 10, 5, 100, 101);
         InHouse partB = new InHouse(2, "Part B", 4.99, 20, 5, 100, 102);
         InHouse partC = new InHouse(3, "Part C", 5.99, 30, 5, 100, 103);
@@ -50,22 +56,24 @@ public class Main extends Application {
         Inventory.addPart(partB);
         Inventory.addPart(partC);
         //Outsourced parts
-        Outsourced partD = new Outsourced(4, "Part D", 6.99, 10, 5, 100, "Part Co.");
-        Outsourced partE = new Outsourced(5, "Part E", 7.99, 20, 5, 100, "Part Co.");
-        Outsourced partF = new Outsourced(6, "Part F", 8.99, 30, 5, 100, "Part Co.");
-        Outsourced partG = new Outsourced(7, "Part G", 9.99, 10, 5, 100, "Part Co.");
-        Outsourced partH = new Outsourced(8, "Part H", 10.99, 20, 5, 100, "Part Co.");
+        Outsourced partD = new Outsourced(4, "Part D", 7.99, 20, 5, 100, "Part Co.");
+        Outsourced partE = new Outsourced(5, "Part E", 8.99, 30, 5, 100, "Part Co.");
+        Outsourced partF = new Outsourced(6, "Part F", 9.99, 10, 5, 100, "Part Co.");
         Inventory.addPart(partD);
         Inventory.addPart(partE);
         Inventory.addPart(partF);
-        Inventory.addPart(partG);
-        Inventory.addPart(partH);
         //Products
         Product productA = new Product(1, "Product A", 3.99, 10, 5, 100);
         Product productB = new Product(2, "Product B", 4.99, 20, 5, 100);
         Product productC = new Product(3, "Product C", 5.99, 30, 5, 100);
+        Product productD = new Product(4, "Product D", 3.99, 10, 5, 100);
+        Product productE = new Product(5, "Product E", 4.99, 20, 5, 100);
+        Product productF = new Product(6, "Product F", 5.99, 30, 5, 100);
         Inventory.addProduct(productA);
         Inventory.addProduct(productB);
         Inventory.addProduct(productC);
+        Inventory.addProduct(productD);
+        Inventory.addProduct(productE);
+        Inventory.addProduct(productF);
     }
 }
